c61104a204c94ffae75a7fac1aa03cc0f6b3edca
Mallory Stammer <mallorystammer2@gmail.com>
2021-12-10 17:48:37 -0600

    updated location, coreCompetency, and positionType classes

--- TechJobsOO/Job.cs
+++ TechJobsOO/Job.cs
@@ -2,6 +2,7 @@
 namespace TechJobsOO
 {
     public class Job
+        //thanks, Sally
     {
         public int Id { get; }
         private static int nextId = 1;
@@ -11,6 +12,12 @@ namespace TechJobsOO
         public Location EmployerLocation { get; set; }
         public PositionType JobType { get; set; }
         public CoreCompetency JobCoreCompetency { get; set; }
+
+        public Job()
+        {
+            Id = nextId;
+            nextId++
+        }
 
         // TODO: Add the two necessary constructors.
 
--- TechJobsOO/Program.cs
+++ TechJobsOO/Program.cs
@@ -7,6 +7,13 @@ namespace TechJobsOO
         static void Main(string[] args)
         {
             /*
+             * Has anyone else used Visual Studio's "extract base class" option? 
+             * It seems useful...
+
+            Todd Romkema  11 hours ago
+            Like, "it does most of the work for you on a large portion of the 'Create a Base Class' and
+            'Extend JobField into Employer' parts of Assignment 2" level of useful
+             * 
             Job job1 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
             Job job2 = new Job("Web Developer", new Employer("LaunchCode"), new Location("St. Louis"), new PositionType("Front-end developer"), new CoreCompetency("JavaScript"));
             Job job3 = new Job("Ice cream tester", new Employer(""), new Location("Home"), new PositionType("UX"), new CoreCompetency("Tasting ability"));
